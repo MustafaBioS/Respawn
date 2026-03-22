4c1d5647ddf801f15f82fd86ba809c57a2f2e538
MustafaBioS <mustaafahany@gmail.com>
2026-03-22 23:06:35 +0200

    ADD - Responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 
 	import { onMount } from 'svelte';
-	import Lenis from 'lenis';
+	// import Lenis from 'lenis';
 
 	let el;
 	let box;
@@ -9,11 +9,11 @@
 	let close;
 	let btn;
 
-	onMount(() => {
-		const lenis = new Lenis({
-			autoRaf: true,
-		});
-	}); // Smooth Scrolling
+	// onMount(() => {
+	// 	const lenis = new Lenis({
+	// 		autoRaf: true,
+	// 	});
+	// }); // Smooth Scrolling
 
 	let started = $state(false)
 
@@ -164,14 +164,14 @@
 
 <svelte:window onkeydown={keyPress} ontouchstart={keyPress}></svelte:window>
 
-<div bind:this={overlay} class="hidden overlay h-screen w-full bg-black opacity-70 fixed z-99"></div>
+<div bind:this={overlay} class="overlay hidden h-screen w-full bg-black opacity-70 fixed z-99"></div>
 
 <div class="absolute flex items-center justify-center h-screen w-full">
-	<div bind:this={box} class="hidden faq flex-col h-165 w-165 rounded-xl bg-[#1e1452] border-[#100933] border-5 z-100 overflow-y-auto">
+	<div bind:this={box} class="faq hidden flex-col h-165 w-165 rounded-xl bg-[#063e59] border-[#073145] border-5 z-100 overflow-y-auto">
 
 		<div class="top flex flex-row items-start justify-between w-full">
-			<h1 class="text-3xl m-10 text-white">Frequently Asked Questions</h1>
-			<button bind:this={close} class="close text-3xl m-10 text-white">X</button>
+			<h1 class="text-4xl m-10 text-white">Frequently Asked Questions</h1>
+			<button bind:this={close} class="close text-4xl m-10 text-white">X</button>
 		</div>
 
 		<div class="questions flex flex-col items-start justify-start w-full h-full">
@@ -192,21 +192,8 @@
 				<span class='hiddenDesc'>Respawn is open to all Hack Clubbers. All teenagers 18 and under, anywhere in the world can participate.</span>
 			</button>
 
-			<button class='q' class:open={openFaqIndex === 2} onclick={() => toggleFAQ(2)}>
-				<div class='q-header'>
-					What are the requirements for each project?
-					<span class='arrow'>▼</span>
-				</div>
-				<span class='hiddenDesc'>
-					<ul>
-						<li>Have open-source code on Github with commits every hour.</li><br>
-						<li>No AI usage at all.</li><br>
-						<li>Reflect the hours and work spent on the project accurately. don't commit fraud!</li>
-					</ul>
-				</span>
-			</button>
 
-			<button class='q' class:open={openFaqIndex === 3} onclick={() => toggleFAQ(3)}>
+			<button class='q' class:open={openFaqIndex === 2} onclick={() => toggleFAQ(2)}>
 				<div class='q-header'>
 					Can i use AI to help make my project?
 					<span class='arrow'>▼</span>
@@ -214,72 +201,85 @@
 				<span class='hiddenDesc'>Only up to <span class="font-extrabold">30%</span> of your project may be AI.</span>
 			</button>
 
-			<button class='q' class:open={openFaqIndex === 4} onclick={() => toggleFAQ(4)}>
+
+			<button class='q' class:open={openFaqIndex === 3} onclick={() => toggleFAQ(3)}>
 				<div class='q-header'>
-					How many projects can i make?
+					Can I submit Respawn projects to any other YSWS?
 					<span class='arrow'>▼</span>
 				</div>
-				<span class='hiddenDesc'>You can make as many projects as you'd like!</span>
+				<span class='hiddenDesc'>No, "Double Dipping" is not allowed, if you submit a project to Respawn then you can not submit it to other Hack Club program.</span>
 			</button>
 
-<!--			<button class='q' class:open={openFaqIndex === 5} onclick={() => toggleFAQ(5)}>-->
-<!--				<div class='q-header'>-->
-<!--					What-->
-<!--					<span class='arrow'>▼</span>-->
-<!--				</div>-->
-<!--				<span class='hiddenDesc'>You can make as many projects as you'd like!</span>-->
-<!--			</button>-->
+			<button class='q' class:open={openFaqIndex === 4} onclick={() => toggleFAQ(4)}>
+				<div class='q-header'>
+					Do I need to have any prior experience?
+					<span class='arrow'>▼</span>
+				</div>
+				<span class='hiddenDesc'>Nope! Respawn is completely beginner friendly and we welcome people with any amount of experience to join and make cool projects!</span>
+			</button>
 
 		</div>
 
 	</div>
 </div>
 
-<!--{#if !started}-->
+{#if !started}
 
-<!--	<div class="startup h-screen w-screen bg-black absolute flex items-center justify-center">-->
-<!--		<h1 class="blink text-white text-[clamp(1rem,5vw,3.5rem)] text-center">{isMobile ? '[ TAP TO START ]' : '[ PRESS ANY KEY ]'}</h1>-->
-<!--	</div>-->
+	<div class="startup h-screen w-screen bg-black absolute flex items-center justify-center">
+		<h1 class="blink text-white text-[clamp(1rem,5vw,3.5rem)] text-center">{isMobile ? '[ TAP TO START ]' : '[ PRESS ANY KEY ]'}</h1>
+	</div>
 
-<!--{:else}-->
+{:else}
 
 
 <div class="con flex h-screen w-full items-center justify-center text-center flex-col gap-5">
 	<h1 class="title font-bold text-5xl text-[#E1C418]">Respawn</h1>
-	<p class="text-white text-l w-[35%] mt-2">Make An Arcade Classic Game With Your Own Twist Added To It, Get Rewards!</p>
+	<p class="text-white min-w-85 w-[75%] max-w-175 mt-2 txt">Make An Arcade Classic Game With Your Own Twist Added To It, Get Rewards!</p>
 	<button title="Button" onclick={RSVP} class="yellow btn w-90 bg-black border-3 border-b-9 h-16 border-[#E1C418] text-white mt-7 cursor-pointer">RSVP</button>
-	<button title="Button" bind:this={btn} onclick={openFAQ} class="blue btn w-90 bg-black border-3 border-b-9 mt-3 h-16 border-[#06aecc] text-white cursor-pointer">FAQ</button>
-	<button title="Button" onclick={shop} class="purple btn w-90 bg-black border-3 mt-3 border-b-9 h-16 border-[#bf5fff] text-white cursor-pointer">Shop</button>
+	<button title="Button" bind:this={btn} onclick={openFAQ} class="blue btn w-90 bg-black border-3 border-b-9 mt-1 h-16 border-[#06aecc] text-white cursor-pointer">FAQ</button>
+	<button title="Button" onclick={shop} class="purple btn w-90 bg-black border-3 mt-1 border-b-9 h-16 border-[#bf5fff] text-white cursor-pointer">Shop</button>
 </div>
 
-<div class="con flex h-screen w-full text-center items-center flex-col">
+<!--<div class="con flex h-screen w-full text-center items-center flex-col">-->
 
-	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center justify-items-center items-center h-full w-full">
+<!--	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center justify-items-center items-center h-full w-full">-->
 
-		<div class="gi h-137.5 w-96 rounded-2xl bg-white flex flex-col items-center z-60">
-			<h1 class="text-3xl mt-17">Build</h1>
-			<div class="flex h-[60%] w-full items-center justify-center">
-				<p class="w-[75%]">Build an arcade classic game with your own twist added to it</p>
-			</div>
-		</div>
-		<div class="gi h-137.5 w-96 rounded-2xl bg-white flex flex-col items-cener z-60">
-			<h1 class="text-3xl mt-17">Ship</h1>
-			<div class="flex h-[60%] w-full items-center justify-center">
-				<p class="w-[75%]">Build an arcade classic game with your own twist added to it</p>
-			</div>
-		</div>
-		<div class="gi h-137.5 w-96 rounded-2xl bg-white flex flex-col items-center md:col-span-2 lg:col-span-1 md:mx-auto z-50">
-			<h1 class="text-3xl mt-17">Get</h1>
-			<div class="flex h-[60%] w-full items-center justify-center">
-				<p class="w-[75%]">Build an arcade classic game with your own twist added to it</p>
-			</div>
-		</div>
-	</div>
-</div>
+<!--		<div class="gi h-137.5 w-96 rounded-2xl bg-white flex flex-col items-center z-60">-->
+<!--			<h1 class="text-3xl mt-17">Build</h1>-->
+<!--			<div class="flex h-[60%] w-full items-center justify-center">-->
+<!--				<p class="w-[75%]">Build an arcade classic game with your own twist added to it</p>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--		<div class="gi h-137.5 w-96 rounded-2xl bg-white flex flex-col items-cener z-60">-->
+<!--			<h1 class="text-3xl mt-17">Ship</h1>-->
+<!--			<div class="flex h-[60%] w-full items-center justify-center">-->
+<!--				<p class="w-[75%]">Ship your game to something like <a href="http://itch.io" target="_blank">itch.io</a> and your code must be open source</p>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--		<div class="gi h-137.5 w-96 rounded-2xl bg-white flex flex-col items-center md:col-span-2 lg:col-span-1 md:mx-auto z-50">-->
+<!--			<h1 class="text-3xl mt-17">Get</h1>-->
+<!--			<div class="flex h-[60%] w-full items-center justify-center">-->
+<!--				<p class="w-[75%]">Get tokens which you can spend in the shop to buy a lot of cool items like console grants!</p>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--	</div>-->
+<!--</div>-->
 
-<!--{/if}-->
+{/if}
 
 <style>
+
+	.btn {
+				width: clamp(17.5rem, 50vw, 22rem);
+			}
+
+	.title {
+			font-size: clamp(2rem, 5vw, 3rem);
+	}
+
+	.txt {
+			font-size: clamp(0.75rem, 2.5vw, 1rem);
+	}
 
 	.close {
 		transition: all 0.3s ease;
